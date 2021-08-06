275b7877e1e7e4f247d65a2363108179d1565935
Henrique Jonas <henrique@2x3.com.br>
2021-08-06 14:50:39 -0300

    add new pac/sedex contracts ids

--- lib/correios/frete/servico.rb
+++ lib/correios/frete/servico.rb
@@ -11,6 +11,7 @@ module Correios
         "41068" => { :type => :pac_com_contrato,            :name => "PAC",            :description => "PAC com contrato"                  },
         "04669" => { :type => :pac_com_contrato_2,          :name => "PAC",            :description => "PAC com contrato"                  },
         "04596" => { :type => :pac_com_contrato_3,          :name => "PAC",            :description => "PAC com contrato"                  },
+        "41211" => { :type => :pac_com_contrato_4,          :name => "PAC",            :description => "PAC com contrato"                  },
         "41300" => { :type => :pac_gf,                      :name => "PAC GF",         :description => "PAC para grandes formatos"         },
         "04014" => { :type => :sedex,                       :name => "SEDEX",          :description => "SEDEX sem contrato"                },
         "40045" => { :type => :sedex_a_cobrar,              :name => "SEDEX a Cobrar", :description => "SEDEX a Cobrar, sem contrato"      },
